c176ef5522a6932614e8a07b0e906106c1966c55
Rahul Shah <rashah1998@yahoo.com>
2022-04-04 11:33:06 -0400

    commit tree 9e7ff631ebcdcc76c7b5ee3d82bd891987116dd3

---
+++ app.rb
@@ -0,0 +1,25 @@
+# app.rb
+
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+
+# print [1, 2, 3]
+
+# puts [1, 2, 3].inspect
+
+# puts [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+# print [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+puts "Hello World!\n"
+
+print "Pass this test, please."
+
+p [1, 2, 3]
